53d37c9d872aea08302116a9d182c654ac4df357
tjinjin <tjinjinprogram@gmail.com>
2016-03-26 11:48:18 +0900

    Round

--- perfect_ruby/chapter5/numeric.rb
+++ perfect_ruby/chapter5/numeric.rb
@@ -42,3 +42,35 @@ Complex(1, 2).real? # false
 %w(Alice Bob Charile).sort{|a, b|
   a.length <=> b.length
 }
+
+# 丸め操作
+## round 四捨五入
+1.4.round # 1
+1.5.round # 2
+-1.5.round # -2
+
+## ceil 自身と等しいか、自身より大きい整数のうち最小のものを返す
+1.4.ceil # 2
+1.1.ceil #2
+-1.4.ceil # -1
+-1.1.ceil # -1
+
+##  floor 自身と等しいか、自身より小さい整数のうち最大のものを返す
+1.5.floor # 1
+-1.4.floor #-2
+
+## truncate 自身と0の間で、自身に最も近い整数を返す
+1.5.truncate # 1
+-1.5.truncate # -1
+
+# 繰り返し処理
+3.step 5 do |num|
+  puts num
+end
+
+## 1.2 1.4 1.6 1.8 2.0と表示
+1.2.step 2.0, 0.2 do |num|
+  puts num
+end
+
+
